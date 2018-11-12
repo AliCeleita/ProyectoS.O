65fb59fc9416fbe10ddc29ccfa42d63bb01b6c7b
Unknown <rojasbeltranadrianapatricia@gmail.com>
2018-11-12 11:23:12 -0500

    corin-adriana

--- Procesos/Program.cs
+++ Procesos/Program.cs
@@ -5,13 +5,28 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+
 
 
 namespace Procesos
 {
-     
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Principal());
+        }
+    }
     static class Funciones
     {
+
         [DllImport("kernel32.dll")]
         static extern IntPtr OpenThread(int dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
         [DllImport("kernel32.dll")]
@@ -44,53 +59,23 @@ namespace Procesos
                 ResumeThread(pOpenThread);
             }
         }
-        public static void Print(this Process process)
-        {
-            Console.WriteLine("{0,8}    {1}", process.Id, process.ProcessName);
-        }
+       
 
-        public static Process CreateProcess(int c)
+        public static Process CreateProcess(string url, string proces, string nombre,List<Info_proceso> pros)
         {
+            Info_proceso informacion = new Info_proceso();  
             ProcessStartInfo procesoPadre = new ProcessStartInfo();
-            switch (c)
-            {
-                case 0:
-                    procesoPadre.FileName = @"C:\Users\AliCeleita\Documents\SistemasOperativos\ProyectosJava\RobinRound\dist\RobinRound.jar";
-                    procesoPadre.Arguments = "RobinRound.jar";
-
-                    break;
-                case 1:
-                    procesoPadre.FileName = @"C:\Users\AliCeleita\Documents\SistemasOperativos\ProyectosJava\JavaApplication1\dist\JavaApplication1.jar";
-                    procesoPadre.Arguments = "JavaApplication1.jar";
-
-                    break;
-                case 2:
-                    procesoPadre.FileName = @"C:\Users\AliCeleita\Documents\SistemasOperativos\ProyectosJava\RobinRound\dist\RobinRound.jar";
-                    procesoPadre.Arguments = "RobinRound.jar";
-
-                    break;
-                /*case 3:
-                    procesoPadre.FileName = @"C:\Users\AliCeleita\Documents\SistemasOperativos\ProyectosJava\RobinRound\dist\RobinRound.jar";
-                    procesoPadre.Arguments = "RobinRound.jar";
-
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;*/
-                default:
-                    Console.Write("No hay mas procesos para ejecutar");
-                    break;
-                    Console.Out.Close();   
-            }
-            
+            procesoPadre.FileName = @url;            
+            procesoPadre.Arguments = proces;
             Process ExecutedProces = new Process();
 
             ExecutedProces.StartInfo = procesoPadre;
             ExecutedProces.Start();
 
-            Console.WriteLine("PID del proceso JAVA = " + ExecutedProces.Id.ToString());
-
+            informacion.Indice = ExecutedProces.Id.ToString();
+            informacion.Nombre = nombre;
+            informacion.Url = url;
+            pros.Add(informacion);
             return ExecutedProces;
         }
 
@@ -109,81 +94,6 @@ namespace Procesos
             return indice;
         }
     }
-    class Program
-    {      
 
-        static void Main(string[] args)
-        {
-
-            List<Process> ColaProcesos = new List<Process>();
-            Console.WriteLine("\n\n=====================proceso Stufff===================");
-            String StringRecv = "";
-            int cont = 0;
-            do {
-
-                Console.Write("\nCommand = ");
-                StringRecv = Console.ReadLine();
-
-                if(StringRecv.Equals("nuevo"))
-                {
-                    ColaProcesos.Add(Funciones.CreateProcess(cont));
-                    cont++;
-                }
-
-                if(StringRecv.Equals("buscar"))
-                {
-                    Console.Write("\t[PID] =");
-                    String IdPRoceso = Console.ReadLine();
-                    if (ColaProcesos.Count > 0)
-                    {
-                        int indice = Funciones.SearchProceso(ColaProcesos, int.Parse(IdPRoceso));
-                        if (indice != -1)
-                        {
-                            // realizar acciones sobre el proceso
-                            Console.WriteLine("\t[Proceso Encontrado]");
-                            Console.Write("\t[Accion] = ");
-                            String Accion = Console.ReadLine();
-
-                            // resume
-                            if (Accion.Equals("resume"))
-                            {
-                                Process Temp = Process.GetProcessById(ColaProcesos[indice].Id);
-                                Funciones.Resume(Temp);
-                                Console.WriteLine("\t[resume] Proceso [" + indice + "]");
-                            }
-                            // pausarlo
-                            if (Accion.Equals("pause"))
-                            {
-                                Console.WriteLine("\tPausa");
-                                Process Temp = Process.GetProcessById(ColaProcesos[indice].Id);
-                                Funciones.Suspend(Temp);
-                                Console.WriteLine("\t[pause] Proceso [" + indice + "]");
-                            }
-                            // cerrarlo
-                            if(Accion.Equals("kill"))
-                            {
-                                Process temp = Process.GetProcessById(ColaProcesos[indice].Id);
-                                temp.Kill();
-                                ColaProcesos.Remove(ColaProcesos[indice]);
-                                Console.WriteLine("\t[kill] Proceso muerto ["+indice+"]");
-                            }
-                            else
-                            {
-                                Console.WriteLine("\t[Error] Accion no encontrada");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("[Error] Indice no encontrado");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("[Error] No Existe lista de proceso");
-                    }
-                }
-
-            }while (!StringRecv.Equals("Salir")) ;
-        }
-    }
+   
 }
--- README.md
+++
@@ -1,1 +0,0 @@
-# ProyectoS.O
